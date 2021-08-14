ebb12d4d881d4b4ba4a0744182b3b4a3979c50ad
Brboric Svetozar <svetozarbrboric@gmail.com>
2021-08-14 12:49:07 +0200

    initial commit

---
+++ src/main/java/com/ftn/elastic/ElasticSearch2021/ElasticSearch2021Application.java
@@ -0,0 +1,13 @@
+package com.ftn.elastic.ElasticSearch2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearch2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearch2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftn/elastic/ElasticSearch2021/ElasticSearch2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.elastic.ElasticSearch2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearch2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
